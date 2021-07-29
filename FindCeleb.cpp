a7959727496a7eaf71dc89be5cbf3656a729e300
sampusegaonkar <sameerpusegaonkar@gmail.com>
2021-07-28 22:14:13 -0400

    Added FindCeleb

---
+++ FindCeleb.cpp
@@ -0,0 +1,34 @@
+
+/*
+Solution 1: Create an indegrees and outdegrees array.
+For each element check if indegrees == N-1 an doutgerees = 0
+Time Complexity: O(V*V), V = Number of people/vertices
+Space Complexity: O(V), V = Number of people/vertices
+
+////////////////////////////////////////////////////////////
+
+Solution 2: Assume person 0 is the celeb and keep updating it with other persons.
+Check if other people know the potential Celeb and if the potentialCeleb knows nobody.
+Time Complexity: O(V),  V = Number of people/vertices
+Space Complexity: O(1)
+
+*/
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int potentialCeleb = 0;
+        for (int i = 1; i < n; i++){
+            if ( knows(potentialCeleb, i)){
+                potentialCeleb = i;
+            }
+        }
+        
+        for (int i = 0; i < n; i++){
+            if ( i == potentialCeleb) continue;
+            if ( knows(potentialCeleb, i) or !knows(i, potentialCeleb)){
+                return -1;
+            }
+        }
+        return potentialCeleb;
+    }
+};
